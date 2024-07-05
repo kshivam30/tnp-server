67ed9a2c81cb34c221309db75cdb9c809c5e748b
kshivam30 <shivamkmishra98@gmail.com>
2024-07-05 21:22:44 +0530

    fix

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
 const getUserRoute = require('./routes/getUser')
 const jobsRoute = require('./routes/jobs');
+const removeJobRoute = require ('./routes/removeJob')
 const getJobsRoute = require('./routes/getJobs');
 const allowCors = require('./allowCors'); // Adjust the path if necessary
 const applyRoute = require('./routes/apply'); 
-
 
 
 const app = express();
@@ -26,13 +26,13 @@ app.use('/register', registerRoute);
 app.use('/login', loginRoute);
 app.use('/getUser', getUserRoute);
 app.use('/addJob', jobsRoute);
+app.use('/removeJob', removeJobRoute);
 app.use('/getJobs', getJobsRoute);
 app.use('/jobs/apply', applyRoute);
 
 
 /* MONGOOSE SETUP */
 const uri = process.env.MONGO_URL;
-console.log('MongoDB URI:', uri); // Debugging line
 mongoose
   .connect(uri)
   .then(() => {
